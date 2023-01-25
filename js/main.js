2171218d2d8d0758164ad14842fcc625dad7b354
Tatiana Osipova <otv120780@gmail.com>
2023-01-25 17:32:20 -0500

    feat: add animation

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.burger');
+const container = document.querySelector('.container');
+
+burger.addEventListener('click', () => {
+    container.classList.toggle('active')
+});
